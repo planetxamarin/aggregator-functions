6e56fdb0a98128867e3949a249d821445e0a0da9
Tomasz Cielecki <tci@trackman.com>
2022-08-30 20:38:37 +0200

    Add azure function for feed aggregator

---
+++ PlanetXamarinAuthors/AuthorsLoader.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using PlanetXamarinAuthors.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace PlanetXamarinAuthors
+{
+    public static class AuthorsLoader
+    {
+        public static async Task<IEnumerable<Author>> GetAllAuthors()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceNames = assembly.GetManifestResourceNames();
+            var authorsResourceNames = resourceNames.Where(res =>
+                res.StartsWith("PlanetXamarinAuthors", StringComparison.OrdinalIgnoreCase) &&
+                res.EndsWith(".json", StringComparison.OrdinalIgnoreCase));
+
+            var authorsTasks = authorsResourceNames.Select(name => ReadAuthor(assembly, name));
+            var authors = await Task.WhenAll(authorsTasks).ConfigureAwait(false);
+            return authors;
+        }
+
+        private static async Task<Author> ReadAuthor(Assembly assembly, string authorResourceName)
+        {
+            using var stream = assembly.GetManifestResourceStream(authorResourceName);
+            using var reader = new StreamReader(stream);
+            var json = await reader.ReadToEndAsync().ConfigureAwait(false);
+            var author = JsonConvert.DeserializeObject<Author>(json);
+            return author;
+        }
+    }
+}
