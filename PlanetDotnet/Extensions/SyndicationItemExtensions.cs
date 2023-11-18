5110b567c0deac526ff47980b6fa749913aa3ab1
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2023-11-18 23:41:28 +0100

    FUNCTIONS: Load Feed

---
+++ PlanetDotnet/Extensions/SyndicationItemExtensions.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) 2023 Planet Dotnet. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Linq;
+using System.ServiceModel.Syndication;
+
+namespace PlanetDotnet.Extensions
+{
+    public static class SyndicationItemExtensions
+    {
+        public static bool ApplyDefaultFilter(this SyndicationItem item)
+        {
+            if (item == null)
+                return false;
+
+            var hasXamarinCategory = false;
+            var hasXamarinKeywords = false;
+
+            if (item.Categories.Count > 0)
+            {
+                hasXamarinCategory = item.Categories.Any(category =>
+                    category.Name.ToLowerInvariant().Contains("xamarin"));
+            }
+
+            if (item.ElementExtensions.Count > 0)
+            {
+                var element = item.ElementExtensions.FirstOrDefault(e => e.OuterName == "keywords");
+                if (element != null)
+                {
+                    var keywords = element.GetObject<string>();
+                    hasXamarinKeywords = keywords.ToLowerInvariant().Contains("xamarin");
+                }
+            }
+
+            var hasXamarinTitle = item.Title?.Text.ToLowerInvariant().Contains("xamarin") ?? false;
+
+            return hasXamarinTitle || hasXamarinCategory || hasXamarinKeywords;
+        }
+
+        public static string ToHtml(this SyndicationContent content)
+        {
+            var textSyndicationContent = content as TextSyndicationContent;
+            if (textSyndicationContent != null)
+            {
+                return textSyndicationContent.Text;
+            }
+
+            return content.ToString();
+        }
+    }
+
+}
--- PlanetDotnet/LoadFeedsFunction.cs
+++ PlanetDotnet/LoadFeedsFunction.cs
@@ -4,37 +4,97 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------
 
+using System;
 using System.IO;
+using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using PlanetDotnet.Authors.Services;
+using PlanetDotnet.Infrastructure;
 
 namespace PlanetDotnet
 {
     public static class LoadFeedsFunction
     {
         [FunctionName("LoadFeedsFunction")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        public static async Task Run(
+            [TimerTrigger("0 0 */1 * * *", RunOnStartup = true)] TimerInfo myTimer,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
-            string name = req.Query["name"];
+            var rssFeedTitle = GetEnvironmentVariable("RssFeedTitle");
+            var rssFeedDescription = GetEnvironmentVariable("RssFeedDescription");
+            var rssFeedUrl = GetEnvironmentVariable("RssFeedUrl");
+            var rssFeedImageUrl = GetEnvironmentVariable("RssFeedImageUrl");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var authors = await AuthorService.GetAllAuthors();
+            var languages = authors.Select(author => author.FeedLanguageCode).Distinct().ToList();
+            languages.Add("mixed");
+            var feedSource =
+                new CombinedFeedSource(
+                    authors,
+                    log,
+                    rssFeedTitle,
+                    rssFeedDescription,
+                    rssFeedUrl,
+                    rssFeedImageUrl);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            var blobConnectString = GetEnvironmentVariable("FeedBlobStorage");
+            var container = new BlobContainerClient(blobConnectString, "feeds");
+            await container.CreateIfNotExistsAsync();
+            await container.SetAccessPolicyAsync(PublicAccessType.Blob);
 
-            return new OkObjectResult(responseMessage);
+            foreach (var language in languages)
+            {
+                try
+                {
+                    log.LogInformation($"Loading {language} combined author feed");
+                    var feed = await feedSource.LoadFeed(null, language);
+                    using var stream = await SerializeFeed(feed);
+                    await UploadBlob(container, stream, language, log);
+                }
+                catch (Exception ex)
+                {
+                    log.LogError(ex, ex.Message);
+                }
+            }
+        }
+
+        private static async Task UploadBlob(BlobContainerClient container, Stream feedStream, string language, ILogger log)
+        {
+            var feedName = $"feed.{language}.rss";
+            var blob = container.GetBlobClient(feedName);
+            await blob.UploadAsync(feedStream, overwrite: true);
+
+            log.LogInformation($"Uploaded {feedName} to {blob.Uri}");
+        }
+
+        private static async Task<Stream> SerializeFeed(SyndicationFeed feed)
+        {
+            var memoryStream = new MemoryStream();
+            using var xmlWriter = XmlWriter.Create(memoryStream, new XmlWriterSettings
+            {
+                Async = true
+            });
+
+            var rssFormatter = new Rss20FeedFormatter(feed);
+            rssFormatter.WriteTo(xmlWriter);
+            await xmlWriter.FlushAsync();
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return memoryStream;
+        }
+
+        private static string GetEnvironmentVariable(string name)
+        {
+            return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
         }
     }
 }
