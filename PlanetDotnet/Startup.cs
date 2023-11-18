0629df002a4e4dfdb94199479b2bb3c136f0387d
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2023-11-18 22:27:36 +0100

    INFRA: Initialize Project

---
+++ PlanetDotnet/Startup.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) 2023 Planet Dotnet. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(PlanetDotnet.Startup))]
+namespace PlanetDotnet
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        { }
+    }
+}
