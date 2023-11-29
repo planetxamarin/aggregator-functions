0a1efa3a7bf5b296dcb7c4aace5d5cb76fc7fcec
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2023-11-29 02:48:48 +0100

    ShouldRetrieveAllAuthorsAsync -> PASS

---
+++ PlanetDotnet.Tests.Unit/Services/Foundations/Authors/AuthorServiceTests.Logic.GetAll.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) 2023 Planet Dotnet. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using PlanetDotnet.Authors.Models.Authors;
+using Xunit;
+
+namespace PlanetDotnet.Tests.Unit.Services.Foundations.Authors
+{
+    public partial class AuthorServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveAllAuthorsAsync()
+        {
+            // given
+            IEnumerable<Author> randomAuthors = CreateRandomAuthors();
+            IEnumerable<Author> storageAuthors = randomAuthors;
+            IEnumerable<Author> expectedAuthors = storageAuthors;
+
+            this.authorBrokerMock.Setup(broker =>
+                broker.GetAllAuthorsAsync())
+                    .ReturnsAsync(expectedAuthors);
+
+            // when
+            var actualAuthors =
+                await this.authorService.RetrieveAllAuthorsAsync();
+
+            // then
+            actualAuthors.Should().BeEquivalentTo(expectedAuthors);
+
+            this.authorBrokerMock.Verify(broker =>
+                broker.GetAllAuthorsAsync(),
+                    Times.Once());
+
+            this.authorBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ PlanetDotnet.Tests.Unit/Services/Foundations/Authors/AuthorServiceTests.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) 2023 Planet Dotnet. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using Moq;
+using PlanetDotnet.Authors.Models.Authors;
+using PlanetDotnet.Brokers.Authors;
+using PlanetDotnet.Services.Foundations.Authors;
+using Tynamix.ObjectFiller;
+
+namespace PlanetDotnet.Tests.Unit.Services.Foundations.Authors
+{
+    public partial class AuthorServiceTests
+    {
+        private readonly Mock<IAuthorBroker> authorBrokerMock;
+        private readonly IAuthorService authorService;
+
+        public AuthorServiceTests()
+        {
+            this.authorBrokerMock = new Mock<IAuthorBroker>();
+
+            this.authorService = new AuthorService(
+                authorBroker: this.authorBrokerMock.Object);
+        }
+
+        private static IEnumerable<Author> CreateRandomAuthors() =>
+             CreateAuthorFiller().Create(count: GetRandomNumber());
+
+        private static Author CreateRandomAuthor() =>
+            CreateAuthorFiller().Create();
+
+        private static Filler<Author> CreateAuthorFiller() => new();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 15).GetValue();
+    }
+}
